17884226645c45229e56bdcda08754c2fa3237c6
Alastair Droop <alastair.droop@york.ac.uk>
2023-01-31 20:04:58 +0000

    Add load_sequence

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::fs::File;
+use std::path::{Path, PathBuf};
+use std::io::{BufRead, BufReader};
+use std::env::var;
+
 fn complement(c: char) -> char {
     match c {
         'A' => 'T',
@@ -18,6 +23,18 @@ fn is_palindrome(s: &str) -> bool {
     }
     let half_len = s.len() / 2_usize;
     s[0..half_len] == reverse_complement(&s[half_len..])
+}
+
+fn load_sequence(path: &Path) -> String {
+    let input_file = File::open(&path).unwrap();
+    let input_buffer = BufReader::new(input_file);
+    input_buffer.lines().filter_map(|l| {
+        let l = l.unwrap();
+        match l.starts_with(">") {
+            false => Some(l),
+            true => None,
+        }
+    }).collect()
 }
 
 fn main() {
@@ -49,5 +66,11 @@ mod tests {
         assert_eq!(is_palindrome("ATAGCTAA"), false);
         assert_eq!(is_palindrome("ATAGCTA"), false);
         assert_eq!(is_palindrome("TGAGTCGATGCAAGGAAATAGAGCGCGCTCTATTTCCTTGCATCGACTCA"), true);
+    }
+
+    #[test]
+    fn test_load_sequence() {
+        let input_path = PathBuf::from(format!("{}/tests/tiny.fasta", var("CARGO_MANIFEST_DIR").unwrap()));
+        assert_eq!(load_sequence(&input_path), "ACATGAGGC");
     }
 }
