5261f555b14bdd26f7f3d2ce3341580c661e4c1a
Alastair Droop <alastair.droop@york.ac.uk>
2023-01-31 20:32:07 +0000

    Clippy and formatter

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::fs::File;
-use std::path::{Path, PathBuf};
-use std::io::{BufRead, BufReader};
-use std::env::var;
 use rayon::prelude::*;
+use std::env::var;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::{Path, PathBuf};
 
 fn complement(c: char) -> char {
     match c {
@@ -14,14 +14,10 @@ fn complement(c: char) -> char {
     }
 }
 
-fn reverse_complement(s: &str) -> String {
-    s.chars().map(|c| complement(c)).rev().collect()
-}
-
 fn is_palindrome(s: &str) -> bool {
-    s.chars().zip(s.chars().rev()).all(|(i, j)|{
-        i == complement(j)
-    })
+    s.chars()
+        .zip(s.chars().rev())
+        .all(|(i, j)| i == complement(j))
 }
 
 fn all_n(s: &str) -> bool {
@@ -29,37 +25,48 @@ fn all_n(s: &str) -> bool {
 }
 
 fn load_sequence(path: &Path) -> String {
-    let input_file = File::open(&path).unwrap();
+    let input_file = File::open(path).unwrap();
     let input_buffer = BufReader::new(input_file);
-    input_buffer.lines().filter_map(|l| {
-        let l = l.unwrap();
-        match l.starts_with(">") {
-            false => Some(l),
-            true => None,
-        }
-    }).collect()
+    input_buffer
+        .lines()
+        .filter_map(|l| {
+            let l = l.unwrap();
+            match l.starts_with('>') {
+                false => Some(l),
+                true => None,
+            }
+        })
+        .collect()
 }
 
 fn main() {
-    rayon::ThreadPoolBuilder::new().num_threads(8).build_global().unwrap();
-    let window_size:usize = 100;
-    let input_path = PathBuf::from(format!("{}/tests/chr1.fasta", var("CARGO_MANIFEST_DIR").unwrap()));
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(8)
+        .build_global()
+        .unwrap();
+    let window_size: usize = 100;
+    let input_path = PathBuf::from(format!(
+        "{}/tests/chr1.fasta",
+        var("CARGO_MANIFEST_DIR").unwrap()
+    ));
     let input_sequence = load_sequence(&input_path);
-    let positions:Vec<usize> = (0_usize..(input_sequence.len() - window_size)).into_par_iter().filter_map(|i| {
-        let slice = &input_sequence[i..(i + window_size)];
-        if all_n(&slice) {
-            return None
-        };
-        match is_palindrome(slice) {
-            false => None,
-            true => Some(i),
-        }
-    }).collect();
+    let positions: Vec<usize> = (0_usize..(input_sequence.len() - window_size))
+        .into_par_iter()
+        .filter_map(|i| {
+            let slice = &input_sequence[i..(i + window_size)];
+            if all_n(slice) {
+                return None;
+            };
+            match is_palindrome(slice) {
+                false => None,
+                true => Some(i),
+            }
+        })
+        .collect();
     println!("{} palindromes found", positions.len());
     for i in positions {
         println!("{}\t{}", i, &input_sequence[i..(i + window_size)]);
     }
-
 }
 
 #[cfg(test)]
@@ -77,21 +84,22 @@ mod tests {
     }
 
     #[test]
-    fn test_reverse_complement() {
-        assert_eq!(reverse_complement("ATAG"), "CTAT");
-    }
-
-    #[test]
     fn test_is_palindrome() {
         assert_eq!(is_palindrome("ATAGCTAT"), true);
         assert_eq!(is_palindrome("ATAGCTAA"), false);
         assert_eq!(is_palindrome("ATAGCTA"), false);
-        assert_eq!(is_palindrome("TGAGTCGATGCAAGGAAATAGAGCGCGCTCTATTTCCTTGCATCGACTCA"), true);
+        assert_eq!(
+            is_palindrome("TGAGTCGATGCAAGGAAATAGAGCGCGCTCTATTTCCTTGCATCGACTCA"),
+            true
+        );
     }
 
     #[test]
     fn test_load_sequence() {
-        let input_path = PathBuf::from(format!("{}/tests/tiny.fasta", var("CARGO_MANIFEST_DIR").unwrap()));
+        let input_path = PathBuf::from(format!(
+            "{}/tests/tiny.fasta",
+            var("CARGO_MANIFEST_DIR").unwrap()
+        ));
         assert_eq!(load_sequence(&input_path), "ACATGAGGC");
     }
 
