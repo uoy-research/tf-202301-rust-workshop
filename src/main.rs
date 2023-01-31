d2e3c8914fa5120c15cdbeebd57516c424627067
Alastair Droop <alastair.droop@york.ac.uk>
2023-01-31 19:50:23 +0000

    Add complement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+fn complement(c: char) -> char {
+    match c {
+        'A' => 'T',
+        'C' => 'G',
+        'G' => 'C',
+        'T' => 'A',
+        _ => 'N',
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_complement() {
+        assert_eq!(complement('A'), 'T');
+        assert_eq!(complement('C'), 'G');
+        assert_eq!(complement('G'), 'C');
+        assert_eq!(complement('T'), 'A');
+        assert_eq!(complement('N'), 'N');
+        assert_eq!(complement('?'), 'N');
+    }
 }
